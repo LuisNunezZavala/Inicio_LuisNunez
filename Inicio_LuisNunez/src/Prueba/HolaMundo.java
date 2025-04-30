535f203e29f318f3d09edd9454ff04dceebb8190
Vadon <Vadon@VASUS>
2025-04-30 12:07:34 +0200

    Prueba2

--- Inicio_LuisNunez/src/Prueba/HolaMundo.java
+++ Inicio_LuisNunez/src/Prueba/HolaMundo.java
@@ -4,6 +4,6 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-		System.out.println("Soy Vadon Rozas");
+		System.out.println("Soy Vadon Rozas ");
 	}
 }
