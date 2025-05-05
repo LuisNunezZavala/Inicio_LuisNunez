65a17564d85f993d60d0838ab7e97a48250fba3c
Luis <Luis@DESKTOP-LUIS>
2025-05-05 10:27:03 +0200

    prueba4

--- Inicio_LuisNunez/src/Prueba/HolaMundo.java
+++ Inicio_LuisNunez/src/Prueba/HolaMundo.java
@@ -5,5 +5,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Soy Vadon Rozas");
+		System.out.println("Hola");
 	}
 }
